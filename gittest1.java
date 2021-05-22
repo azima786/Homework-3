fabede2fda7ee8f3311ab586be6e1e91985c124f
azima786 <azima786@gmail.com>
2021-05-22 12:32:12 -0500

    3rd Commit

---
+++ gittest1.java
@@ -0,0 +1,17 @@
+package Homework2;
+
+public class gittest1 {
+
+	public static void main(String[] args) {
+		printMe();
+		printMe1();
+	}
+
+	public static void printMe() {
+		System.out.println("GitTest");
+		
+	}
+
+	public static void printMe1() {
+		System.out.println("GitTest1");}
+}
